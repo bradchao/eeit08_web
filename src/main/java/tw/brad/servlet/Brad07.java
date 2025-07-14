8bb61bc0c77da17aefde11eac9bf97dc5ff8e8ef
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-14 15:46:05 +0800

    07141545

--- src/main/java/tw/brad/servlet/Brad07.java
+++ src/main/java/tw/brad/servlet/Brad07.java
@@ -9,16 +9,28 @@ import java.io.IOException;
 
 @WebServlet("/Brad07")
 public class Brad07 extends HttpServlet {
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		System.out.println("doGet");
-//	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		
 		String account = request.getParameter("account");
 		String passwd = request.getParameter("passwd");
 		String name = request.getParameter("name");
+		String[] interests = request.getParameterValues("interest");
 		System.out.printf("%s : %s : %s\n", account, passwd, name);
+		if (interests != null) {
+			for (String interest: interests) {
+				System.out.println(interest);
+			}
+		}else {
+			System.out.println("NO");
+		}
+		
+		
+		
 	}
 
 }
---
+++ src/main/java/tw/brad/servlet/Brad08.java
@@ -0,0 +1,56 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Brad08")
+public class Brad08 extends HttpServlet {
+	public Brad08() {
+		System.out.println("Brad08()");
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init(ServletConfig)");
+		super.init(config);
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		System.out.println("service(ServletRequest,ServletResponse)");
+		if (request instanceof HttpServletRequest) {
+			System.out.println("Y");
+		}else {
+			System.out.println("N");
+		}
+		super.service(request, response);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service(HttpServletRequest,HttpServletResponse)");
+		//super.service(arg0, arg1);
+		
+		String method =  request.getMethod();
+		System.out.println(method);
+		
+	}
+
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+		super.init();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
+	}
+}
