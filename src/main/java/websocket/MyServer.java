9410f18a676e402c0746be81b9fcfa538ed9e461
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-15 15:40:51 +0800

    07151540

--- src/main/java/websocket/MyServer.java
+++ src/main/java/websocket/MyServer.java
@@ -1,5 +1,8 @@
 package websocket;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
 import jakarta.websocket.OnMessage;
@@ -9,13 +12,25 @@ import jakarta.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/myserver")
 public class MyServer {
+	private static HashSet<Session> sessions;
+	private static HashMap<String, Session> users;
+	
 	public MyServer() {
 		System.out.println("MyServer()");
+		if (sessions == null) {
+			sessions = new HashSet<Session>();
+			users = new HashMap<String, Session>();
+		}
 	}
 	
 	@OnOpen
 	public void onOpen(Session session) {
 		System.out.println("OnOpen");
+		
+		if (sessions.add(session)) {
+			users.put(session.getId(), session);
+			System.out.println("count:" + sessions.size());
+		}
 	}
 	
 	@OnMessage
@@ -26,6 +41,9 @@ public class MyServer {
 	@OnClose
 	public void onClose(Session session) {
 		System.out.println("OnClose");
+		users.remove(session.getId());
+		sessions.remove(session);
+		System.out.println("count:" + sessions.size());
 	}
 	
 	@OnError
