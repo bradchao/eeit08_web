d4222b8c6a7323ccd14c2a4e8baaaa83349dbec2
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-16 14:01:10 +0800

    07161401

---
+++ src/main/java/tw/brad/servlet/Register.java
@@ -0,0 +1,18 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+    public Register() {
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
